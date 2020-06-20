6e011fc476b30db71f79572c4f179010f881bcd7
Bru-Drago <bruna_drago@hotmail.com>
2020-06-20 16:39:25 -0300

    Implementado o get na API

--- Teste_BrunaDrago/View Controller/DetailViewController.swift
+++ Teste_BrunaDrago/View Controller/DetailViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UIViewController{
     
+    
+
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var clientNameLabel: UILabel!
     @IBOutlet weak var accountNumberLabel: UILabel!
     @IBOutlet weak var accountBalanceLabel: UILabel!
     
+    var operations = [StatementList]()
+    var detail:DetailService?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Atribuir o delegate e o datasource
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //Chamando o método que irá fazer o GET dos dados das operações
+        getData()
     }
     
 
@@ -31,6 +40,55 @@ class DetailViewController: UIViewController {
         
     }
     
+    //Mark: - Método Get
+    func getData(){
+            let urlJsonString = "https://bank-app-test.herokuapp.com/api/statements/1"
+            
+            let url = URL(string: urlJsonString)
+            
+            guard url != nil else {
+                print("Erro na URL")
+                return
+            }
+            URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                guard let data = data else {return}
+                
+                do{
+                    self.detail =  try JSONDecoder().decode(DetailService.self, from: data)
+                    self.operations = self.detail?.statementList as! [StatementList]
+                    
+                }catch let jsonErr{
+                    print("Error : \(jsonErr)")
+                }
+                DispatchQueue.main.async {
+                    print(self.detail?.statementList.count)
+                               self.tableView.reloadData()
+                           }
+                
+            }.resume()
+            
+            
+        }
+    }
 
-
+extension DetailViewController:UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return operations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)as! TransactionsTableViewCell
+        
+        cell.operationLabel.text = operations[indexPath.row].title
+        cell.dateLabel.text = operations[indexPath.row].date
+        cell.descriptionLabel.text = operations[indexPath.row].desc
+        
+        let amount = String(format: "%.2f", operations[indexPath.row].value!)
+        cell.amountLabel.text = "R$\(amount)"
+        
+        
+        return cell
+    }
 }
